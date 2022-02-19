dd061ed8632e73be51252e668cd6e4669feaf4c9
Kevin Godell <kevin.godell@gmail.com>
2022-02-19 08:54:58 -0600

    remove filename param

--- tests/aac/getParams.js
+++ tests/aac/getParams.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const getParams = ({ profile, filename }) => {
+const getParams = ({ profile }) => {
   return [
     '-hide_banner',
     '-y',
@@ -22,7 +22,7 @@ const getParams = ({ profile, filename }) => {
     'mp4',
     '-movflags',
     '+frag_keyframe+empty_moov+default_base_moof',
-    filename,
+    'pipe:1',
   ];
 };
 
