d09f61fe5688752f758288f6769a7098a7c98955
Kevin Godell <kevin.godell@gmail.com>
2022-02-19 10:54:09 -0600

    add more samples

--- tests/aac/config.js
+++ tests/aac/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const profiles = ['aac_main', 'aac_low', 'aac_he', 'aac_eld', 'aac_ld', 'aac_he_v2'];
+const profiles = ['aac_main', 'aac_low', 'mpeg2_aac_low', 'aac_ssr', 'main10', 'mpeg2_aac_he', 'aac_he', 'aac_eld', 'aac_ld', 'aac_he_v2'];
 
 module.exports = {
   profiles,
